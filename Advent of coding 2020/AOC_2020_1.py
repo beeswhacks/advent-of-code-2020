c8a3f0199e852fe0480f5605bb430cc01e2e5d9e
Jack Preston <je.preston20@gmail.com>
2021-10-26 21:21:47 +0100

    First challenge complete!

---
+++ Advent of coding 2020/AOC_2020_1.py
@@ -0,0 +1,48 @@
+# initialise puzzle input
+puzzle_input = []
+
+# get puzzle input from text file
+with open('/Users/jack/Documents/Scratch/AOC_2020_1_input.txt') as in_file:
+
+    # use readlines to split text file into list
+    in_file_list = in_file.readlines()
+
+    # remove newline characters and convert string values to integers
+    for i in in_file_list:
+        puzzle_input.append(int(i.replace('\n','')))
+
+# for each item in the puzzle input
+for i in range(len(puzzle_input)):
+    # loop through the remaining puzzle items
+    for j in range(len(puzzle_input)):
+        # check if the sum of the two elements is 2020, and that the two elements aren't identical
+        if i != j and (puzzle_input[i] + puzzle_input[j]) == 2020:
+            first_value = puzzle_input[i]
+            second_value = puzzle_input[j]            
+            answer = puzzle_input[i] * puzzle_input[j]
+            break
+
+print("First value: ", first_value)
+print("Second value: ", second_value)
+print("Answer: ", answer)
+print("\n")
+
+
+# for each item in the puzzle input
+for i in range(len(puzzle_input)):
+    # loop through the remaining puzzle items
+    for j in range(len(puzzle_input)):
+        for k in range(len(puzzle_input)):
+            # check if the sum of the two elements is 2020, and that the two elements aren't identical
+            if i != j and j != k and i != k and (puzzle_input[i] + puzzle_input[j] + puzzle_input[k]) == 2020:
+                first_value = puzzle_input[i]
+                second_value = puzzle_input[j]  
+                third_value = puzzle_input[k]            
+                answer = puzzle_input[i] * puzzle_input[j] * puzzle_input[k]
+                break
+
+print("First value: ", first_value)
+print("Second value: ", second_value)
+print("Third value: ", third_value)
+print("Answer: ", answer)
+print("\n")
