34a51fca13fafa1dbea2242a83bb6ed79fb0d4c2
Jack Preston <je.preston20@gmail.com>
2021-11-07 20:02:17 +0000

    Solved part 1

--- Advent of coding 2020/AOC_2020_2.py
+++ Advent of coding 2020/AOC_2020_2.py
@@ -1,20 +1,28 @@
-# firstly, need to get a list where each element is a list containing: lower lim, upper lim, letter, and password
-puzzle_input = []
-
+# read password policies into a list so that i can loop through them
 with open('/Users/jack/Repos/side/Advent of coding 2020/AOC_2020_2_input.txt') as in_file:
     in_file_list = in_file.readlines()
     
+# remove newline characters
 for i in in_file_list:
-    puzzle_input.append(i.replace('\n',''))
-
-puzzle_input_split_raw = []
-puzzle_input_split = []
-for count, i in enumerate(puzzle_input):
-    puzzle_input_split_raw.append(i.split())
-    puzzle_input_split[0] = puzzle_input_split_raw[0]
-#    list_of_lists[count] = split(i)
+    i = i.replace('\n','')
         
-print(puzzle_input_split)
-# loop through passwords
+# parse each password policy into a dictionary object
+puzzle_input = []
+for i in in_file_list:
+    puzzle_input.append({
+    "lower_lim"     : int(i[:i.index('-')]),
+    "upper_lim"     : int(i[i.index('-') + 1 : i.index(' ')]),
+    "target_letter" : i[i.index(':') - 1 : i.index(':')],
+    "password"      : i[i.index(':') + 1 :]
+    })
 
-# if the password meets the requirements, increment by 1
+# loop through password policies
+valid_password_count = 0
+for i in puzzle_input:
+    # count instances of target letter in each policy
+    target_letter_count = i["password"].count(i["target_letter"])
+    # count how many passwords meet the policy requirements
+    if target_letter_count >= i["lower_lim"] and target_letter_count <= i["upper_lim"]:
+        valid_password_count += 1
+
+print(valid_password_count, "passwords are valid.")
