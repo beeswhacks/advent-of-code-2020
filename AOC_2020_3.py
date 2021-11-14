250f6db1fbfa962ae3820c702f2c998a0df58fc1
Jack Preston <je.preston20@gmail.com>
2021-11-14 09:21:12 +0000

    Add solution for part 2

--- AOC_2020_3.py
+++ AOC_2020_3.py
@@ -1,3 +1,6 @@
+# import product from math library to multiply list elements together
+from math import prod
+
 # read in puzzle input
 with open('/Users/jack/Repos/advent-of-code-2020/AOC_2020_3_input.txt') as in_file:
     in_file_list = in_file.readlines()
@@ -11,23 +14,52 @@ row_length = len(puzzle_input[0])
 last_element = row_length - 1
 number_of_rows = len(puzzle_input)
 
-# set off from position (0, 0), so we start counting from first move of (+3, +1)
-x_position = 3
-y_position = 1
-tree_count = 0
+# list of different increment schemes
+increments = [
+    {
+        "x_increment" : 1,
+        "y_increment" : 1
+    },
+    {
+        "x_increment" : 3,
+        "y_increment" : 1
+    },
+    {
+        "x_increment" : 5,
+        "y_increment" : 1
+    },
+    {
+        "x_increment" : 7,
+        "y_increment" : 1
+    },
+    {
+        "x_increment" : 1,
+        "y_increment" : 2
+    },
+]
 
-while y_position <= number_of_rows - 1:
-    if puzzle_input[y_position][x_position] == '#':
-        tree_count += 1
+tree_count_list = []
+# loop through different increment schemes
+for i in increments:
+    x_position = i["x_increment"]
+    y_position = i["y_increment"]
+    tree_count = 0
 
-    # if x_position is not within 3 of the end of the row, increase x_position by 3
-    if x_position <= last_element - 3:
-        x_position += 3
-    # otherwise if x_position is within 3 of the end, move up by 3 and back by row length to start from beginning of row
-    else:
-        x_position += 3
-        x_position -= row_length
+    while y_position <= number_of_rows - 1:
+        if puzzle_input[y_position][x_position] == '#':
+            tree_count += 1
 
-    y_position += 1
+        # if x_position is not within 3 of the end of the row, increase x_position by 3
+        if x_position <= last_element - i["x_increment"]:
+            x_position += i["x_increment"]
+        # otherwise if x_position is within 3 of the end, move up by 3 and back by row length to start from beginning of row
+        else:
+            x_position += i["x_increment"]
+            x_position -= row_length
 
-print("Number of trees: " + str(tree_count))
+        y_position += i["y_increment"]
+
+    print("Number of trees encountered for pattern (" + str(i["x_increment"]) + ", " + str(i["y_increment"])  + ") = " + str(tree_count))    
+    tree_count_list.append(tree_count)
+
+print("Number of trees: " + str(prod(tree_count_list)))
